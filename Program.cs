11e835ff488f9c5e19fcd88ff7e755e3b6566920
RohitM79420 <rohitmaskar@gmail.com>
2023-11-03 16:13:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using ADO.NET;
+internal class Program
+{
+    private static IConfiguration _iconfiguration;
+    private static void Main(string[] args)
+    {
+        GetAppSettingsFile();
+        PrintProduct();
+    }
+    static void GetAppSettingsFile()
+    {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+        _iconfiguration = builder.Build();
+    }
+
+
+    static void PrintProduct()
+    {
+        Productlayer obj = new Productlayer( _iconfiguration);
+        obj.Products();
+    }
+}
